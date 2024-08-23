965931a30c652567e05f7516d73afa17e2a81e25
João Cláudio <leiloka46@gmail.com>
2024-08-23 18:00:52 -0300

    Department entity class

---
+++ src/main/java/org/example/application/Main.java
@@ -0,0 +1,11 @@
+package org.example.application;
+
+import org.example.model.entities.Department;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Department dp = new Department(1, "Books");
+        System.out.println(dp);
+    }
+}
